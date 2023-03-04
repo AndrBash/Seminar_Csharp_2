ce944871a64bec5eed01ff9c184582f0c9d925f1
Andrey <aba_2007@mail.ru>
2023-03-04 17:19:32 +0300

    Add Example_005

---
+++ Vebinar_27.02/Example_005/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход два числа 
+// и проверяет, является ли одно число квадратом другого
+Console.Clear();
+System.Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+bool isSquare = Math.Pow(firstNumber, 2) == secondNumber || Math.Pow(secondNumber, 2) == firstNumber;
+if (isSquare)
+    System.Console.WriteLine("Yes");
+else
+    System.Console.WriteLine("No");
