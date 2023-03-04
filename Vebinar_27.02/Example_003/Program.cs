62dc0728589fe631c81f1eba98f1daa37a79afe6
Andrey <aba_2007@mail.ru>
2023-03-04 16:42:28 +0300

    Add Example_003 and file .gitignore

---
+++ Vebinar_27.02/Example_003/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая будет принимать на вход два числа и выводить, 
+//является ли второе число кратным первому. Если второе число не кратно числу первому, 
+//то программа выводит остаток от деления
+Console.Clear();
+System.Console.WriteLine("Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber % secondNumber == 0)
+{
+    System.Console.WriteLine("Второе число кратное первому");
+}
+else
+{
+    System.Console.WriteLine($"Не кратно= {firstNumber % secondNumber}");
+}
