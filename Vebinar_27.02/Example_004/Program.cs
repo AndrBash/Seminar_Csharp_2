3a587aab9562b2984fd40830638b661e747b36d5
Andrey <aba_2007@mail.ru>
2023-03-04 17:08:00 +0300

    Add Example_004

---
+++ Vebinar_27.02/Example_004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+Console.Clear();
+System.Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number % 7 == 0 && Number % 23 == 0)
+{
+    System.Console.WriteLine("Кратно");
+}
+else
+{
+    System.Console.WriteLine("Не кратно");
+}
