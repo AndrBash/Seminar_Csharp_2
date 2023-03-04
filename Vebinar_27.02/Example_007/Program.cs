8d1108916f32a42aa04c3c9155800d2e8ef6e558
Andrey <aba_2007@mail.ru>
2023-03-04 17:41:13 +0300

    Add Example_007

---
+++ Vebinar_27.02/Example_007/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вычислить : 1+2+4+8+...+ 2 в 10 степени.
+int n = 2;
+int sum = 0;
+for (int i = 0; i <= 10; i++)
+{
+    sum += (int)Math.Pow(n, i);
+    System.Console.WriteLine(Math.Pow(n, i));
+}
+System.Console.WriteLine(sum);
