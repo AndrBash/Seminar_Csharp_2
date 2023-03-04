546d64296c11f06031b3c3e06483ac6597cab063
Andrey <aba_2007@mail.ru>
2023-03-04 15:33:27 +0300

    Add Example_001 and Example_002

---
+++ Vebinar_27.02/Example_001/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+Console.Clear();
+Random rnd = new Random();
+int number = rnd.Next(1,100);
+//System.Console.WriteLine(number);
+//int number = new Random().Next(10,100); - можно использовать если нет множества рандомов
+int firstDigit = number / 10;
+int lastDigit = number % 10;
+int maxDigit = Math.Max(firstDigit, lastDigit);
+System.Console.WriteLine(number);
+System.Console.WriteLine(maxDigit);
+//System.Console.WriteLine(Math.Max(firstDigit, lastDigit)); - вариант сокращения записи
---
+++ Vebinar_27.02/Example_002/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+Random rnd = new Random();
+int number = rnd.Next(100, 1000);
+int firstDigit = (number / 100) * 10;
+int lastDigit = number % 10;
+int result = firstDigit+lastDigit;
+System.Console.WriteLine(number);
+System.Console.WriteLine(result);
