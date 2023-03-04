2f11c2666f34b60e4909ed3b97913fa0c6342fe6
Andrey <aba_2007@mail.ru>
2023-03-04 17:33:38 +0300

    Add Example_006

---
+++ Vebinar_27.02/Example_006/Program.cs
@@ -0,0 +1,10 @@
+﻿// дано натуральное n. вычислить: 1 + 1/2 + 1/3 + 1/4 + ... + 1/n.
+System.Console.WriteLine("ВВедите число");
+int n = Convert.ToInt32(Console.ReadLine());
+float sum = 0;
+for (int i = 1; i <= n; i++)
+{
+    sum += (float)1/(float)i;
+    System.Console.WriteLine((float)1/(float)i);
+}
+System.Console.WriteLine(sum);
