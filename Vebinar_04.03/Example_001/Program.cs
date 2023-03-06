dbe454a8807bec02d95d1d270f70c9108ffe39d0
Andrey <aba_2007@mail.ru>
2023-03-06 23:34:32 +0300

    Add folder "Vebinar_04.03 with Example_001 and Homework_001"

---
+++ Vebinar_04.03/Example_001/Program.cs
@@ -0,0 +1,17 @@
+﻿//Сложение 2-х чисел через функцию
+Console.Clear();
+void Sum (int num1, int num2)
+{
+    System.Console.WriteLine(num1 + num2);
+}
+int inputNumberWithMessage(string message)
+{
+    System.Console.WriteLine(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+//System.Console.WriteLine("Введите число 1: ");
+int num1 = inputNumberWithMessage("Введите число 1");
+//System.Console.WriteLine("Введите число 2: ");
+int num2 = inputNumberWithMessage("Введите число 2");
+Sum(num1, num2);
---
+++ Vebinar_04.03/Homework_001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B
+Console.Clear();
+void Sum (int A, int B)
+{
+    double Stepen = Math.Pow(A, B);
+    System.Console.WriteLine(Stepen);
+}
+int inputNumberWithMessage(string message)
+{
+    System.Console.WriteLine(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int A = inputNumberWithMessage("Введите число A");
+int B = inputNumberWithMessage("Введите число B");
+System.Console.WriteLine("Result A^B is:");
+Sum(A, B);
