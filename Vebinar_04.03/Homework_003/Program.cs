f1e5b6b001ed520ede227c158e87ee4e31488f7c
Andrey <aba_2007@mail.ru>
2023-03-11 21:11:01 +0300

    Homework_003 done

--- Vebinar_04.03/Homework_003/Program.cs
+++ Vebinar_04.03/Homework_003/Program.cs
@@ -1,2 +1,33 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-Console.WriteLine("Hello, World!");
+Console.Clear();
+int ResultCode(string message)
+{
+    System.Console.Write(message);
+    string valueIn = System.Console.ReadLine();
+    int result = int.Parse(valueIn);
+    return result;
+}
+int[] NewArray(int Length, int minValue, int maxValue)
+{
+    int[] array = new int[Length];
+    Random values = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = values.Next(minValue, maxValue +1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}");
+}
+int Length = ResultCode("Введите длину массива: ");
+int min = ResultCode("Начальное занчение: ");
+int max = ResultCode("Конечнре значение: ");
+int[] array = NewArray(Length, min, max);
+PrintArray(array);
