7e4f47d9eb2cf1000fa43fee900b57aedbf4e01e
Andrey <aba_2007@mail.ru>
2023-03-11 20:50:53 +0300

    Homework_002 done

---
+++ Vebinar_04.03/Homework_002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+int ResultCode(string message)
+{
+    System.Console.Write(message);
+    string valueIn = System.Console.ReadLine();
+    int result = int.Parse(valueIn);
+    return result;
+}
+int SummaDigits(int index)
+{
+    int result = 0;
+    while(index > 0)
+    {
+        result = result + index % 10;
+        index = index / 10;
+    }
+    return result;
+}
+int index = ResultCode("Введите число: ");
+System.Console.WriteLine($"Сумма чисел в числе {index} = {SummaDigits(index)}");
