60749b4bcadbee89cee38789942776fd5d8180fe
Andrey <aba_2007@mail.ru>
2023-03-12 15:27:37 +0300

    Exercises_23 done

---
+++ Homework_till_25.03/Exercises_23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+System.Console.WriteLine("ВВедите число");
+int n = Convert.ToInt32(Console.ReadLine());
+float kub = 0;
+for (int i = 1; i <= n; i++)
+{
+    kub = (float)i*(float)i*(float)i;
+}
+System.Console.WriteLine(kub);
