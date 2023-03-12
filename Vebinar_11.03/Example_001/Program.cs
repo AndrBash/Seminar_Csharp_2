fadbc6fd31935f5264e68098ad9fc6b0b23408c3
Andrey <aba_2007@mail.ru>
2023-03-12 18:21:15 +0300

    Add Vebinar_11.03 and Example_001 about array

---
+++ Vebinar_11.03/Example_001/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+Console.Clear();
+
+int[] CreateAndFillArray(int size, int leftRangeValue, int rightRangeValue)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(leftRangeValue, rightRangeValue);
+    }
+    return array;
+}
+void PrintArrayInConsole(int[] array)
+{
+    System.Console.WriteLine(string.Join(",", array));
+}
+(int moreThenZero, int lessThenZero) GetSummsNumbersLessAndMoreThenZero(int[] array)
+{
+    (int moreThenZero, int lessThenZero) amounts = (0, 0);
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            amounts.moreThenZero += array[i];
+        else
+        {
+            amounts.lessThenZero += array[i];
+        }
+    }
+    return amounts;
+}
+int size = 12;
+int[] array = CreateAndFillArray(size, -9, 10);
+PrintArrayInConsole(array);
+(int moreThenZero, int lessThenZero) amounts = GetSummsNumbersLessAndMoreThenZero(array);
+System.Console.WriteLine($"Больше 0 = {amounts.moreThenZero}, меньше 0 = {amounts.lessThenZero}");
