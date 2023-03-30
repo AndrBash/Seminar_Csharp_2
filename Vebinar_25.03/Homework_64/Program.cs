f4dfe2e30f61617cc6a72fded891f5490016a852
Andrey <aba_2007@mail.ru>
2023-03-30 22:51:30 +0300

    Add Vebinar_25.03 and Homework_64 is done

---
+++ Vebinar_25.03/Homework_64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+Natural(number, count);
+void Natural(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        Natural(n, count + 1);
+        System.Console.Write(count + " ");
+    }
+}
